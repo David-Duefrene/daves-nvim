4c3707a59bb2a1cb8df0767c89a7e28692b6f5c9
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-08-23 20:08:34 -0600

    fixed save take 2

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   -- add symbols-outline
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "jose-elias-alvarez/typescript.nvim" },
     opts = {
       -- options for vim.diagnostic.config()
       diagnostics = {
@@ -13,7 +14,7 @@ return {
           prefix = "●",
           -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
           -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-          -- prefix = "icons",
+          prefix = "icons",
         },
         severity_sort = true,
       },
@@ -21,15 +22,15 @@ return {
       -- Be aware that you also will need to properly configure your LSP server to
       -- provide the inlay hints.
       inlay_hints = {
-        enabled = false,
+        enabled = true,
       },
       -- add any global capabilities here
       capabilities = {},
       -- Automatically format on save
-      autoformat = true,
+      autoformat = false,
       -- Enable this to show formatters used in a notification
       -- Useful for debugging formatter issues
-      format_notify = false,
+      format_notify = true,
       -- options for vim.lsp.buf.format
       -- `bufnr` and `filter` is handled by the LazyVim formatter,
       -- but can be also overridden when specified
@@ -40,6 +41,33 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
+        ---@type lspconfig.options.tsserver
+        tsserver = {
+          keys = {
+            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+          },
+          settings = {
+            eslint = { settings = { workingDirectory = { mode = "auto" } } },
+            typescript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            javascript = {
+              format = {
+                indentSize = vim.o.shiftwidth,
+                convertTabsToSpaces = vim.o.expandtab,
+                tabSize = vim.o.tabstop,
+              },
+            },
+            completions = {
+              completeFunctionCalls = true,
+            },
+          },
+        },
         jsonls = {},
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
@@ -65,10 +93,27 @@ return {
       setup = {
         -- example to setup with typescript.nvim
         tsserver = function(_, opts)
-          --   require("typescript").setup({ server = opts })
+          require("typescript").setup({ server = opts })
           return true
         end,
-        -- Specify * to use this function as a fallback for any server
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              -- if not require("lazyvim.plugins.lsp.format").enabled() then
+              -- exit early if autoformat is not enabled
+              --  return
+              -- end
+
+              local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end,
+          })
+        end,
         ["*"] = function(server, opts) end,
       },
     },
