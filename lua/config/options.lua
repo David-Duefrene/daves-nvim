7296f0dcb5c3689143324d32a36d6d4040115494
David Duefrene <1645298+David-Duefrene@users.noreply.github.com>
2023-08-23 18:34:53 -0600

    fixed saving issue
    no longer have to save twice

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -59,4 +59,4 @@ vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a 
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
-vim.o.noexpandtab = true
+vim.noexpandtab = true
